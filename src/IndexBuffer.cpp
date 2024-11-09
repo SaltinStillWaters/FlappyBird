7c12319bf9012f029b113343a38db7721eea8afd
raiu <paraguaraphael0913@gmail.com>
2024-11-09 21:49:50 +0800

    - Added file reading to IndexBuffers.

--- src/IndexBuffer.cpp
+++ src/IndexBuffer.cpp
@@ -1,8 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #include "IndexBuffer.h"
 
 IndexBuffer::IndexBuffer(std::string filename, GLenum usage)
     : BufferObj(GL_ELEMENT_ARRAY_BUFFER, usage) {
-        
+    std::ifstream fileReader;
+    fileReader.open("data/" + filename);
+    if (!fileReader)
+        throw std::runtime_error("Vertex file not found");
+
+    std::vector<GLuint> bufferData;
+    GLuint input;
+
+    while (fileReader >> input) {
+        bufferData.push_back(input);
+    }
+
+    count = bufferData.size();
+    bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(GLuint),
+                 bufferData.data(), usage);
 }
 
-GLsizei IndexBuffer::getIndexCount() { return indexCount; }
+IndexBuffer::IndexBuffer(const void *data, GLsizeiptr size, GLuint count,
+                         GLenum usage)
+    : BufferObj(data, size, GL_ELEMENT_ARRAY_BUFFER, count, usage) {
+    IndexBuffer::type = type;
+}
