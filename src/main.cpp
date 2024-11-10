4e4d5b966b15e53932919c8e490ce58357214c30
Ralph Clarence C. Bello <82042428+RalphClarence1126@users.noreply.github.com>
2024-11-10 16:25:14 +0800

    Basic bird movement

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,16 @@ void display();
 void init();
 void idle();
 void cleanup();
-
+void jump(int key, int state, int x, int y);
 ArrayBuffer *buffer;
 IndexBuffer *ibuffer;
 DrawableObj *obj;
 AttribFormat *attribFormat;
 
 GLfloat rotate = 0.f;
+GLfloat yOffSet = 0.f; 
+GLfloat fly = 0.f;
+bool isFlying = false;
 
 int main(int argcp, char **argv) {
     glutInit(&argcp, argv);
@@ -30,6 +33,7 @@ int main(int argcp, char **argv) {
 
     glutDisplayFunc(display);
     glutIdleFunc(idle);
+    glutMouseFunc(jump);
     glutMainLoop();
 
     cleanup();
@@ -42,12 +46,12 @@ void init() {
 
     attribFormat = new AttribFormat();
     attribFormat->addAttrib<GLfloat>(2, GL_VERTEX_ARRAY);
-    attribFormat->addAttrib<GLubyte>(3, GL_COLOR_ARRAY);
+    attribFormat->addAttrib<GLfloat>(4, GL_COLOR_ARRAY);
 
-    buffer = new ArrayBuffer("vertices.data", attribFormat);
-    ibuffer = new IndexBuffer("indices.data");
+    buffer = new ArrayBuffer("bird.data", attribFormat);
+    // ibuffer = new IndexBuffer("indices.data");
 
-    obj = new DrawableObj(GL_QUADS, buffer, ibuffer);
+    obj = new DrawableObj(GL_QUADS, buffer);
 }
 
 void display() {
@@ -59,15 +63,32 @@ void display() {
 }
 
 void idle() {
-    obj->setRotation(rotate);
-    rotate += rotate >= 360.f ? -rotate : 1.f;
+    obj->setOffset(0, yOffSet);
+    if (isFlying) {
+        if (fly >= 0.5f) {
+            isFlying = false;
+            fly = 0;
+        }
+        yOffSet += 0.025f;
+        fly += 0.01f;
+    } else {
+        yOffSet -= 0.025f;
+    }
     Sleep(1000 / 60);
     glutPostRedisplay();
 }
 
+
+void jump(int key, int state, int x, int y){
+    if (key == GLUT_LEFT_BUTTON && state == GLUT_DOWN) { 
+        isFlying = true;
+    }
+}
+
+
 void cleanup() {
     delete buffer;
-    delete ibuffer;
+    //delete ibuffer;
     delete obj;
     delete attribFormat;
 
